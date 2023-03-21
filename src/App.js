5d1576ae819ab860eb1011abde64c7afff89f70b
Sorin Lupastean <lupasteansorin1@gmail.com>
2023-03-21 03:04:17 +0200

    Started implementing functions for registration and authenticatio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Suspense } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import CreateAccountPage from "./pages/CreateAccountPage/CreateAccountPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
@@ -14,27 +15,27 @@ function App() {
   console.log(pathname);
 
   return (
-    <>
-      <Routes>
-        <Route exact path="/" element={<CreateAccountPage />} />
-        <Route path="/sign-in-page" element={<SignInPage />} />
-        <Route path="/sign-up-page" element={<CreateAccountPage />} />
-      </Routes>
-      {!(pathname === "/sign-up-page" || pathname === "/sign-in-page") && (
-        <div className="pagina">
-          <Sidebar />
-          <div className="continut">
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/my-forms" element={<MyForms />} />
-              <Route path="/submissions" element={<Submissions />} />
-              <Route path="/my-forms/:id" element={<CreateForm />} />
-              <Route path="/submissions/:id" element={<FillForm />} />
-            </Routes>
-          </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="pagina">
+        {!(
+          pathname === "/sign-up-page" ||
+          pathname === "/sign-in-page" ||
+          pathname === "/"
+        ) && <Sidebar />}
+        <div className="continut">
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/my-forms" element={<MyForms />} />
+            <Route path="/submissions" element={<Submissions />} />
+            <Route path="/my-forms/:id" element={<CreateForm />} />
+            <Route path="/submissions/:id" element={<FillForm />} />
+            <Route exact path="/" element={<SignInPage />} />
+            <Route path="/sign-in-page" element={<SignInPage />} />
+            <Route path="/sign-up-page" element={<CreateAccountPage />} />
+          </Routes>
         </div>
-      )}
-    </>
+      </div>
+    </Suspense>
   );
 }
 
